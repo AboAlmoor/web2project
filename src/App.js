c30423eb8c8ad857b61149d32980a238a721dbb8
Mohamad-husain <s12218954@stu.najah.edu>
2024-05-03 00:03:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LogIn from './Component/LogIn/LogIn';
+import CreateAcount from './Component/CreateAcount/CreateAcount';
+import FindAccount from './Component/ForgetPassword/ForgetPassword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/LogIn" element={<LogIn />} />
+        <Route index path="/CreateAcount" element={<CreateAcount/>} />
+        <Route index path="/ForgetPassword" element={<FindAccount/>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
