f91af3e5420b6bbf9ef650a22ffa8b3838f2e9e1
Yazan Ashour <149838519+Yazan-Ashour@users.noreply.github.com>
2024-05-05 14:29:54 +0300

    YazanAshour Navbar

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Navbar.css';
+import image_logo from "./logo.webp";
+import image_profile from "./Profile.jpeg";
+// import { Link } from 'react-router-dom';
+
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <img className="logoImage-Navbar" src={image_logo} alt="" />
+            </div>
+            <div className="links">
+
+                {/* <Link to='/places'>places</Link>
+            <Link to='/Home Page'>Home Page</Link>
+            <Link to='/Guide'>Guide</Link> */}
+
+                <a href="/#" className="Navbar-link">HomePage</a>
+                <a href="/#" className="Navbar-link">Places</a>
+                <a href="/#" className="Navbar-link">Guide</a>
+                <a href="/#" className="Navbar-link">knownTheUnknown</a>
+
+            </div>
+            <div className="profile">
+                <img className="profileImage-Navbar" src={image_profile} alt="" />
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
